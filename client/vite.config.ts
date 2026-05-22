cc04981e7e666c3f4e94a2d310018da9caa5038a
Martin Grillo <martin.grillo@gmail.com>
2026-05-22 08:39:10 -0500

    PDF de cotizaciones 3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('jspdf')) {
+              return 'jspdf';
+            }
+            if (id.includes('html2canvas')) {
+              return 'html2canvas';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
